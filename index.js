09d6cc9860aa8b0040c0ec2320911c8683db1098
Philemon Kasereka <kmboghaphilemon@gmail.com>
2026-05-20 10:47:44 +0300

    Exercise 3.17: update phone number via PUT

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.put('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndUpdate(
     request.params.id,
     { name, number },
-    { new: true, runValidators: true, context: 'query' }
+    { returnDocument: 'after', runValidators: true, context: 'query' }
   )
     .then(updatedPerson => {
       if (updatedPerson) {
